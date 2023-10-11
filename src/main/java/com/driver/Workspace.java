ee638ac966a22de5d97fedfa8e7385e95edd9b5c
Arjun chand <chandarjun10@gmail.com>
2023-10-11 20:00:48 +0530

    21nd commit

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,8 +4,9 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Workspace extends Gmail{
 
@@ -14,14 +15,13 @@ public class Workspace extends Gmail{
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
         super(emailId,Integer.MAX_VALUE);
-        calendar = new ArrayList<>();
+        calendar=new ArrayList<>();
 
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
         calendar.add(meeting);
-
     }
 
     public int findMaxMeetings(){
@@ -29,20 +29,21 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        if (calendar.isEmpty())
-            return 0;
-        Collections.sort(calendar, (m1, m2) -> m1.getEndTime().compareTo(m2.getEndTime()));
-        int maxMeetings = 1;
-        Meeting currentMeeting = calendar.get(0);
-        for (int i = 1; i < calendar.size(); i++) {
-            Meeting nextMeeting = calendar.get(i);
 
-            if (nextMeeting.getStartTime().compareTo(currentMeeting.getEndTime()) >= 0) {
-                // The next meeting starts after the current one ends
+        Collections.sort(calendar, (m1, m2) -> m1.getEndTime().compareTo(m2.getEndTime()));
+
+        int maxMeetings = 0;
+        Meeting prevMeeting = null;
+
+        for (Meeting currentMeeting : calendar) {
+            if (prevMeeting == null || currentMeeting.getStartTime().compareTo(prevMeeting.getEndTime()) > 0) {
+                // If the current meeting starts after or at the same time as the previous meeting ends, attend it
                 maxMeetings++;
-                currentMeeting = nextMeeting;
+                prevMeeting = currentMeeting;
             }
         }
+
         return maxMeetings;
+
     }
 }
