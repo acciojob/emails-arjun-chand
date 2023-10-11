52defc7440deeb047d83f7d729aa4b8ea2e4bde5
Arjun chand <chandarjun10@gmail.com>
2023-10-11 20:10:38 +0530

    4 commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,7 +25,7 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(oldPassword == password){
+        if(oldPassword.equals(password)){
             if(validatePassword(newPassword)){
                 password = newPassword;
             }
