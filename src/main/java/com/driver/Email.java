24d730b962a73a9a881613a745c857e473ceee8e
Arjun chand <chandarjun10@gmail.com>
2023-10-11 20:12:37 +0530

    5 commit

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,20 +25,42 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
-        if(oldPassword.equals(password)){
-            if(validatePassword(newPassword)){
-                password = newPassword;
+        if(newPassword.length()>=8){
+            int upperCaseCount=0;
+            for(int i=0;i<newPassword.length();i++){
+                if(newPassword.charAt(i)>='A' && newPassword.charAt(i)<='Z'){
+                    upperCaseCount++;
+                }
+            }
+            if(upperCaseCount>=1){
+                int lowerCaseCount=0;
+                for(int i=0;i<newPassword.length();i++){
+                    if(newPassword.charAt(i)>='a' && newPassword.charAt(i)<='z'){
+                        lowerCaseCount++;
+                    }
+                }
+                if(lowerCaseCount>=1){
+                    int digitCount=0;
+                    for(int i=0;i<newPassword.length();i++){
+                        if(newPassword.charAt(i)>='0' && newPassword.charAt(i)<='9'){
+                            digitCount++;
+                        }
+                    }
+                    if(digitCount>=1){
+                        int specialCharacter=0;
+                        for(int i=0;i<newPassword.length();i++){
+                            if(!(newPassword.charAt(i)>='0' && newPassword.charAt(i)<='9') &&!(newPassword.charAt(i)>='a' && newPassword.charAt(i)<='z') && !(newPassword.charAt(i)>='A' && newPassword.charAt(i)<='Z')){
+                                specialCharacter++;
+                            }
+                        }
+                        if(specialCharacter>=1){
+                            if(oldPassword.equals(password)){
+                                password=newPassword;
+                            }
+                        }
+                    }
+                }
             }
         }
-    }
-    private boolean validatePassword(String newPassword){
-        if((newPassword.length() >= 8) &&
-                newPassword.matches(".*[A-Z].*") &&
-                newPassword.matches(".*[a-z].*") &&
-                newPassword.matches(".*//d.*") &&
-                newPassword.matches(".*[^A-za-z0-9].*"))
-            return true;
-        return false;
-
     }
 }
